85348707ede7c876389eb7c159b91ba004cd021e
fahjin <yongfahjin@hotmail.com>
2021-07-01 22:25:52 +0800

    SEI repo

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,5 +1,31 @@
+import firebase from "../database/firebaseDB";
+import { useState, useEffect, useRef, FlatList, View } from "react";
+import { Route } from "react-router";
+
 function Todo() {
-  return <div>todo</div>;
+  const [notes, setNotes] = useState([]);
+  const [text, setText] = useState("");
+  const inputRef = useRef();
+  const db = firebase.firestore().collection("todos");
+  useEffect(() => {
+    const unsubscribe = db.orderBy("created").onSnapshot((collection) => {
+      const updatedNotes = collection.docs.map((doc) => {
+        const noteObject = { ...doc.data(), id: doc.id };
+      });
+      setNotes(updatedNotes);
+    });
+    return () => {
+      unsubscribe();
+    };
+  }, []);
+
+  const deleteNote = (id) => {
+    console.log("deleting" + id);
+
+    db.doc(id).delete();
+  };
+
+  return <div>todos</div>;
 }
 
 export default Todo;
