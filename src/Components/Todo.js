376bdd7fcf878a51460614ac457af8a700df72ee
fahjin <yongfahjin@hotmail.com>
2021-07-02 09:12:11 +0800

    comment out todo first

--- src/Components/Todo.js
+++ src/Components/Todo.js
@@ -1,29 +1,29 @@
-import firebase from "../database/firebaseDB";
-import { useState, useEffect, useRef, FlatList, View } from "react";
-import { Route } from "react-router";
+// import firebase from "../database/firebaseDB";
+// import { useState, useEffect, useRef, FlatList, View } from "react";
+// import { Route } from "react-router";
 
 function Todo() {
-  const [notes, setNotes] = useState([]);
-  const [text, setText] = useState("");
-  const inputRef = useRef();
-  const db = firebase.firestore().collection("todos");
-  useEffect(() => {
-    const unsubscribe = db.orderBy("created").onSnapshot((collection) => {
-      const updatedNotes = collection.docs.map((doc) => {
-        const noteObject = { ...doc.data(), id: doc.id };
-      });
-      setNotes(updatedNotes);
-    });
-    return () => {
-      unsubscribe();
-    };
-  }, []);
+  // const [notes, setNotes] = useState([]);
+  // const [text, setText] = useState("");
+  // const inputRef = useRef();
+  // const db = firebase.firestore().collection("todos");
+  // useEffect(() => {
+  //   const unsubscribe = db.orderBy("created").onSnapshot((collection) => {
+  //     const updatedNotes = collection.docs.map((doc) => {
+  //       const noteObject = { ...doc.data(), id: doc.id };
+  //     });
+  //     setNotes(updatedNotes);
+  //   });
+  //   return () => {
+  //     unsubscribe();
+  //   };
+  // }, []);
 
-  const deleteNote = (id) => {
-    console.log("deleting" + id);
+  // const deleteNote = (id) => {
+  //   console.log("deleting" + id);
 
-    db.doc(id).delete();
-  };
+  //   db.doc(id).delete();
+  // };
 
   return <div>todos</div>;
 }
