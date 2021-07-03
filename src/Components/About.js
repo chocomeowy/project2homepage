4cbf2573bbaf04115783359d1d05230a1ac1e642
fahjin <yongfahjin@hotmail.com>
2021-07-03 16:14:37 +0800

    change to accordian on about page

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,129 +1,164 @@
-import Card from "@material-ui/core/Card";
-import CardActionArea from "@material-ui/core/CardActionArea";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
-import { makeStyles } from "@material-ui/core/styles";
-import Typography from "@material-ui/core/Typography";
 import React from "react";
-
-const useStyles = makeStyles({
+import { withStyles } from "@material-ui/core/styles";
+import MuiAccordion from "@material-ui/core/Accordion";
+import MuiAccordionSummary from "@material-ui/core/AccordionSummary";
+import MuiAccordionDetails from "@material-ui/core/AccordionDetails";
+import Typography from "@material-ui/core/Typography";
+const Accordion = withStyles({
   root: {
-    maxWidth: 330,
+    border: "1px solid rgba(0, 0, 0, .125)",
+    boxShadow: "none",
+    "&:not(:last-child)": {
+      borderBottom: 0,
+    },
+    "&:before": {
+      display: "none",
+    },
+    "&$expanded": {
+      margin: "auto",
+    },
   },
-});
+  expanded: {},
+})(MuiAccordion);
+
+const AccordionSummary = withStyles({
+  root: {
+    backgroundColor: "rgba(0, 0, 0, .03)",
+    borderBottom: "1px solid rgba(0, 0, 0, .125)",
+    marginBottom: -1,
+    minHeight: 56,
+    "&$expanded": {
+      minHeight: 56,
+    },
+  },
+  content: {
+    "&$expanded": {
+      margin: "10px 0",
+    },
+  },
+  expanded: {},
+})(MuiAccordionSummary);
+
+const AccordionDetails = withStyles((theme) => ({
+  root: {
+    padding: theme.spacing(2),
+  },
+}))(MuiAccordionDetails);
 
 const About = () => {
-  const classes = useStyles();
+  const [expanded, setExpanded] = React.useState("panel1");
+
+  const handleChange = (panel) => (event, newExpanded) => {
+    setExpanded(newExpanded ? panel : false);
+  };
+
   return (
-    <>
-      <div className="center-card">
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Cats picture"
-              height="140"
-              img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/440px-Felis_silvestris_catus_lying_on_rice_straw.jpg"
-              title="Cats picture"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Cat
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                The cat (Felis catus) is a domestic species of small carnivorous
-                mammal.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Cats picture"
-              height="140"
-              img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/440px-Felis_silvestris_catus_lying_on_rice_straw.jpg"
-              title="Cats picture"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Cat
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                The cat (Felis catus) is a domestic species of small carnivorous
-                mammal.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Cats picture"
-              height="140"
-              img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/440px-Felis_silvestris_catus_lying_on_rice_straw.jpg"
-              title="Cats picture"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Cat
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                The cat (Felis catus) is a domestic species of small carnivorous
-                mammal.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Cats picture"
-              height="140"
-              img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/440px-Felis_silvestris_catus_lying_on_rice_straw.jpg"
-              title="Cats picture"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Cat
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                The cat (Felis catus) is a domestic species of small carnivorous
-                mammal.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-        <Card className={classes.root}>
-          <CardActionArea>
-            <CardMedia
-              component="img"
-              alt="Cats picture"
-              height="140"
-              img
-              src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Felis_silvestris_catus_lying_on_rice_straw.jpg/440px-Felis_silvestris_catus_lying_on_rice_straw.jpg"
-              title="Cats picture"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="h2">
-                Cat
-              </Typography>
-              <Typography variant="body2" color="textSecondary" component="p">
-                The cat (Felis catus) is a domestic species of small carnivorous
-                mammal.
-              </Typography>
-            </CardContent>
-          </CardActionArea>
-        </Card>
-      </div>
-    </>
+    <div>
+      <Accordion
+        square
+        expanded={expanded === "panel1"}
+        onChange={handleChange("panel1")}
+      >
+        <AccordionSummary aria-controls="panel1d-content" id="panel1d-header">
+          <Typography>Welcome to my Home Page!</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            This is part of my project during my learnings in General Assembly
+            as a student in Software Engineering Immersive Course!
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel2"}
+        onChange={handleChange("panel2")}
+      >
+        <AccordionSummary aria-controls="panel2d-content" id="panel2d-header">
+          <Typography>Credits to the APIs</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <p>Cat Pic 1 https://thatcopy.pw/catapi/rest/ </p>
+            <p>Cat Pic 2 https://aws.random.cat/meow </p>
+            <p>Random Quotes https://api.quotable.io/random </p>
+            <p>
+              Weather forecast for data.gov.sg
+              https://data.gov.sg/dataset/weather-forecast
+            </p>
+            <p>Material UI from https://material-ui.com/</p>
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel4"}
+        onChange={handleChange("panel4")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Collapsible Group Item #3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Collapsible Group Item #3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Collapsible Group Item #3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+      <Accordion
+        square
+        expanded={expanded === "panel3"}
+        onChange={handleChange("panel3")}
+      >
+        <AccordionSummary aria-controls="panel3d-content" id="panel3d-header">
+          <Typography>Collapsible Group Item #3</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
+            malesuada lacus ex, sit amet blandit leo lobortis eget. Lorem ipsum
+            dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada
+            lacus ex, sit amet blandit leo lobortis eget.
+          </Typography>
+        </AccordionDetails>
+      </Accordion>
+    </div>
   );
 };
 
