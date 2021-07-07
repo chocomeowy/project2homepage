71e15edbfa1c25a2df1d7f00f79b44a8e0a80194
fahjin <yongfahjin@hotmail.com>
2021-07-07 13:02:48 +0800

    hide API keys

--- src/database/firebaseDB.js
+++ src/database/firebaseDB.js
@@ -2,12 +2,12 @@ import firebase from "firebase/app";
 import "firebase/firestore";
 
 var firebaseConfig = {
-  apiKey: "AIzaSyArPyf3YVWWNcahkX_TTY3DTW7T0SGY95k",
-  authDomain: "gatodo-ed53a.firebaseapp.com",
-  projectId: "gatodo-ed53a",
-  storageBucket: "gatodo-ed53a.appspot.com",
-  messagingSenderId: "422326067127",
-  appId: "1:422326067127:web:b9e27ab3e8fd69f869d232",
+  apiKey: process.env.REACT_APP_FIREBASE_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 firebase.initializeApp(firebaseConfig);
